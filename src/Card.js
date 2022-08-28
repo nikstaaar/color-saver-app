e5a1e653d1c578d80bf2bc5cafec27edb5f042ca
Niklas Ostwald <niktjawalla922@gmail.com>
2022-08-28 19:07:41 +0200

    added delete and modify functionality

--- src/Card.js
+++ src/Card.js
@@ -1,53 +1,82 @@
-import "./Card.css"
-import React, {useState} from 'react'
+import {nanoid} from 'nanoid';
+import './Card.css';
+import React, {useState} from 'react';
 
-export default function Card () {
-
-
-   const [colors ,setColor] = useState([{ 
-    id: 1,
-    color: "black",
-    hexCode: "#000000"
-},{ 
-    id: 2,
-    color: "orange",
-    hexCode: "#ff6600"
-},{ 
-    id: 3,
-    color: "red",
-    hexCode: "#ee2020"
-}
-] 
-   
-   )
-    
-    return ( 
+export default function Card() {
+  const [colorState, setColorState] = useState('#ffffff');
+  const [colors, setColor] = useState([]);
+  return (
     <>
-        
+      <form
+        onSubmit={event => {
+          event.preventDefault();
 
-    <form onSubmit={(event) => {
-        // event.target
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const formValues = Object.fromEntries(formData);
-       setColor([...colors, {
-            id: formValues.colorCode,
-            color: "black",
-            hexCode: formValues.colorCode}]);
-}}
->   <input type ="color"></input>
-    <input type = "text" placeholder = "#ffffff" name = "colorCode"></input>
-    <button type = "submit">submit</button> 
-    </form>
-    
-    <ul>
-    {colors.map ((color) => {return (
-    <li key = {color.hexCode}>
-    <div className = "Card" style={{ border: "5px solid " + color.hexCode }} onClick = {() => {
-        navigator.clipboard.writeText(color.hexCode)}}>{color.hexCode} is this color: {color.color}</div> 
-    </li>)
-})}
-    </ul>
+          setColor([
+            ...colors,
+            {
+              id: nanoid(),
+              hexCode: colorState,
+            },
+          ]);
+        }}
+      >
+        <input
+          type="color"
+          value={colorState}
+          onChange={event => {
+            setColorState(event.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          value={colorState}
+          onChange={event => {
+            setColorState(event.target.value);
+          }}
+        ></input>
+        <button type="submit">submit</button>
+      </form>
+
+      <ul className="card-list">
+        {colors.map(color => {
+          return (
+            <li key={color.id}>
+              <div
+                className="Card"
+                style={{background: color.hexCode}}
+                onClick={() => {
+                  navigator.clipboard.writeText(color.hexCode);
+                }}
+              >
+                <input
+                  type="color"
+                  defaultValue={color.hexCode}
+                  onChange={event => {
+                    setColor(
+                      colors.map(colors => {
+                        if (
+                          colors.id === color.id &&
+                          event.target.value.length === 7
+                        ) {
+                          return {...color, hexCode: event.target.value};
+                        } else return colors;
+                      }),
+                    );
+                  }}
+                ></input>
+                <button
+                  onClick={() => {
+                    setColor(colors.filter(colors => colors.id !== color.id));
+                  }}
+                >
+                  X
+                </button>
+                <div className="code-box">{color.hexCode}</div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
-    )
-  }
+  );
+}
